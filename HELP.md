9d20256de402431b00611a17965cb0475911c7cd
Aidos <aidos@paygate.kz>
2023-09-18 11:46:54 +0600

    first commit

---
+++ HELP.md
@@ -0,0 +1,4 @@
+
+# Pet project "Cars"
+
+### Web app using Spring MVC and Spring Boot, Hibernate, Liquibase
---
+++ src/main/java/kz/job4j/cars/CarsApplication.java
@@ -0,0 +1,13 @@
+package kz.job4j.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsApplication.class, args);
+	}
+
+}
